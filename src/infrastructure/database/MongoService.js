c7f09734d7bf451a212b957f771e5e4bec9d066b
Alex Peh <alex.peh@archerlogic.com>
2021-06-25 17:16:28 +0800

    CHANGE: Add docker file

--- src/infrastructure/database/MongoService.js
+++ src/infrastructure/database/MongoService.js
@@ -10,8 +10,8 @@ module.exports = class MongoService extends DBService {
 
     async initDatabase() {
         const dbName = "urlshortener_db";
-        //const url = `mongodb://${process.env.MONGO_HOSTNAME}:27017/${dbName}`; //mongoserver is service name of mongo in dockers
-        const url = `mongodb://192.168.1.103:27017/${dbName}`; //mongoserver is service name of mongo in dockers
+        const url = `mongodb://${process.env.MONGO_HOSTNAME}:27017/${dbName}`; //mongoserver is service name of mongo in dockers
+        //const url = `mongodb://192.168.1.103:27017/${dbName}`; //mongoserver is service name of mongo in dockers
 
         return setTimeout(() => {
             return mongoose.connect(url, { useNewUrlParser: true })
@@ -19,15 +19,11 @@ module.exports = class MongoService extends DBService {
                 console.log("Mongo Database created!");
 
                 await this.urlRepository.deleteAll();
-                //await this.urlRepository.add('1234', '5678').then(data => console.log('data', data));
-                //await this.urlRepository.getByCode('5678').then(data => console.log('data', data));
-                //await this.urlRepository.getByUrl('5678').then(data => console.log('data', data));
-                //await this.urlRepository.getAllCodes().then(data => console.log('data', data));
             })
             .catch(error => {
                 throw error;
             });
-        }, 3000); //delay 10 seconds to let mongo server to get ready
+        }, 10000); //delay 10 seconds to let mongo server to get ready
         //console.log("Mongo Database created!");
     }
 };
